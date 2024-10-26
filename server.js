448940adf9ca83b6f89ac89c356f857016519a57
buwanikaveesha <buwanikaveesha@gmail.com>
2024-10-26 15:05:34 +0530

    install cors and config

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MY_DB_URL)
 
 
 app.use(express.json());
+app.use(cors());
 
 
 app.use('/api', userRoutes);
