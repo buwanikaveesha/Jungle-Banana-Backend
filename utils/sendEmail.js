daad3008493757d224510ff5504c19d2ccba78c0
buwanikaveesha <buwanikaveesha@gmail.com>
2024-12-04 13:39:17 +0530

    fix the bugs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -33,7 +33,7 @@ const sendEmail = async (to, subject, text) => {
         await transporter.sendMail(mailOptions);
         console.log('Email sent successfully!');
     } catch (error) {
-        // Log error if sending fails
+
         console.error('Error sending email:', error);
         throw new Error('Failed to send email: ' + error.message);
     }
